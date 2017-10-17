142e1eab7e95afcbe198ad3a31d53bd704c3e570
xylong <416319808@qq.com>
2017-10-17 14:22:20 +0800

    一次读一条

--- qsbk.py
+++ qsbk.py
@@ -12,6 +12,8 @@ import re
 class QSBK(object):
 	"""糗事百科爬虫"""
 	def __init__(self):
+		self.enable = False
+		self.stories = []
 		self.paegIndex = 1
 		self.url = 'https://www.qiushibaike.com/hot/page/'
 		self.user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
@@ -51,7 +53,40 @@ class QSBK(object):
 				stains.append([stain[0].strip(), text.strip(), stain[2].strip(), stain[3].strip()])
 		return stains
 
+	# 加载并提取页面的内容
+	def loadPage(self):
+		if self.enable == True:
+			# 如果当前未看的页数少于2页，则加载新一页
+			if len(self.stories) < 2:
+				# 获取新一页
+				stories = self.getStains(self.paegIndex)
+				if stories:
+					self.stories.append(stories)
+					self.paegIndex += 1
+
+	# 每次打印一个段子					
+	def getOne(self, stains, page):
+		for stain in stains:
+			_input = input()
+			self.loadPage()
+			if _input == 'q':
+				self.enable = False
+				return
+			print('当前第:%s页\n发布人:%s\n内容:%s\n点赞数:%s\n' % (page, stain[0], stain[1], stain[3]))
+
+
+	def start(self):
+		print('正在读取糗事百科，按回车查看新段子，q退出')
+		self.enable = True
+		self.loadPage()
+		nowPage = 0
+		while self.enable:
+			if len(self.stories) > 0:
+				stories = self.stories[0]
+				nowPage += 1
+				del self.stories[0]
+				self.getOne(stories, nowPage)
+
 
 qq = QSBK()
-res = qq.getStains(1)
-print(res)
+qq.start()
