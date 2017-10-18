305270b21e8a600c040efd02e7e6d3dcf9a5bede
xylong <416319808@qq.com>
2017-10-18 16:26:13 +0800

    获取相册id

--- tmm.py
+++ tmm.py
@@ -17,10 +17,13 @@ class TMM(object):
     def __init__(self):
         self.code = 'gbk'   # 编码格式
         self.url = 'https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8'  # 美人库
+
         # 个人中心(爱秀)
         self.ai_show_url = 'https://mm.taobao.com/self/aiShow.htm?userId=%s'
         self.album_url = 'https://mm.taobao.com/self/album/open_album_list.htm?_charset=utf-8&user_id%20='  # 相册列表
         self.album_photo_url = 'https://mm.taobao.com/self/album/album_photo_list.htm?user_id=%s&album_id=%s'   # 具体相册
+
+        self.pattern = {'album': r'<h4>.*?album_id=(.*?)&'}
 
     # 获取淘女郎list
     def get_tstar_list(self):
@@ -40,13 +43,12 @@ class TMM(object):
         print(res)
 
     # 获取相册列表
-    def get_album_list(self, userId):
+    def get_album_ids(self, userId):
         url = self.album_url + str(userId)
         req = request.Request(url)
         res = self.visit(req)
-        l = re.findall(r'class="mm-first" href="//(.*?)"', res)
-        for item in l:
-            print(item)
+        pattern = self.compile(self.pattern['album'])
+        return re.findall(pattern, res)
 
     # 获取相册照片
     def get_album_photos(self, user_id, album_id):
@@ -62,8 +64,11 @@ class TMM(object):
             if f.status == 200:
                 return f.read().decode('gbk').encode('utf-8').decode('utf-8')
 
-    def mkdir(self):
-        pass
+    # 创建目录
+    def mkdir(self, path):
+        '''没有才创建'''
+        if not os.path.exists(path):
+            os.mkdir(path)
 
     # 保存模特信息
     def save_profile(self):
@@ -73,9 +78,12 @@ class TMM(object):
     def save_imgs(self):
         pass
 
+    # 预编译正则
+    def compile(self, pattern=None):
+        return re.compile(pattern, re.S)
+
     def run(self):
-        s = self.get_tstar_list()
-        print(s)
+        self.get_album_ids(646632716)
 
 
 mm = TMM()
